8c6ba6c9af1f9e60883886fffe50a60198a47071
Kalp Thakkar <42941658+kalpthakkar@users.noreply.github.com>
2020-10-31 01:22:18 +0530

    Updates

---
+++ precache-manifest.ef13d5ba09f3403b0e74ab4f7bff6bd0.js
@@ -0,0 +1,45 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e0880f3c3b439469efd424ca37e101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ef80ede2a883eec232",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e0556b53d2409debe44e",
+    "url": "/static/css/main.ec59f1de.chunk.css"
+  },
+  {
+    "revision": "04ef80ede2a883eec232",
+    "url": "/static/js/2.f466c297.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.f466c297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0556b53d2409debe44e",
+    "url": "/static/js/main.614243ff.chunk.js"
+  },
+  {
+    "revision": "c9b0a537ac9221472b6e",
+    "url": "/static/js/runtime-main.30657a9b.js"
+  },
+  {
+    "url": "/static/media/5bc234e37ee12adc26918eb88e5e4ec4.457e291f.woff2"
+  },
+  {
+    "url": "/static/media/5bc234e37ee12adc26918eb88e5e4ec4.5bc234e3.ttf"
+  },
+  {
+    "url": "/static/media/5bc234e37ee12adc26918eb88e5e4ec4.c2b97114.woff"
+  },
+  {
+    "url": "/static/media/5bc234e37ee12adc26918eb88e5e4ec4.ed080e96.svg"
+  },
+  {
+    "url": "/static/media/5bc234e37ee12adc26918eb88e5e4ec4.f855ed85.eot"
+  }
+]);
